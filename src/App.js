dd3c8bb7c10923d36c8173fc76f3cc3f319388fa
sowmik-sec <sowmik.sec@gmail.com>
2023-02-16 12:37:17 +0600

    some react-bootstrap component added and tested lightly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import CartGroup from "./components/CardGroup/CartGroup";
+import { Button, Spinner } from "react-bootstrap";
+import GridCard from "./components/GridCard/GridCard";
+import ColorSchemesExample from "./components/Navbar/Navbar";
+import BasicExample from "./components/Toast/Toast";
 
 function App() {
   return (
     <div className="App">
+      <ColorSchemesExample></ColorSchemesExample>
       <div className="spinner-border text-danger" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
       <CartGroup></CartGroup>
+      <Button variant="danger">Bootstrap Button</Button>
+      <Spinner animation="grow" variant="danger" />
+      <GridCard></GridCard>
+      <BasicExample></BasicExample>
     </div>
   );
 }
